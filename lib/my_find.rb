b1b72aa26d17d7f7b40c1a44362763b6f2b7f378
Ifeoluwa  Akinremi-Wade <ifeoluwa.wade@gmail.com>
2020-04-24 00:04:27 +0000

    Automatically backed up by Learn

--- lib/my_find.rb
+++ lib/my_find.rb
@@ -4,7 +4,8 @@ def my_find(collection)
 new_values = []
 i = 0
   while i < collection.length
-  new_values << yield(collection[i])
+  if yield(collection[i])
+    return yield(collection[i])
     i += 1
   end
 end
